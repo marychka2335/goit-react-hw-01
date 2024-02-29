c9f359841e05d2874c4f3fc3089b6f8ed59f70ee
marychka2335 <marychka2335@icloud.com>
2024-02-29 11:57:49 +0200

    third commit fo hw (change routes to css modules

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import css from '../FriendListItem/FriendListItem.module.css'
+import css from './FriendListItem.module.css';
 
 
 export default function FriendListItem({isOnline, name, avatar}) {
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import css from '../Profile/Profile.module.css'
+import css from './Profile.module.css';
 
 export default function Profile ({username, tag, location, avatar, stats}) {
     return (
